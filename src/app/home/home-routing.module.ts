22555d1dd9c0e9ea2d1deecd304b6a2c36451a8e
jamharu30 <jamharu30@yahoo.com>
2020-05-21 15:14:25 +0800

    Updated home

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,12 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+
 import { HomePage } from './home.page';
+import { IndexComponent } from './profile/index/index.component';
+import { FriendComponent } from './friend/friendIndex/friend.component';
+import { InitialComponent } from './about/info/initial.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomePage,
-  }
+    children: [
+      {
+        path: 'profiles',
+        component: IndexComponent,
+      },
+      {
+        path: 'friends',
+        component: FriendComponent,
+      },
+      {
+        path: 'about',
+        component: InitialComponent,
+      },
+    ]
+  },
 ];
 
 @NgModule({
